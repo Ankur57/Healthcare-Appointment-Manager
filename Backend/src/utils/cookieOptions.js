80e6716765c1e7905d2b802deff8bb277671cc33
Ankur Bajpai <ankurbajpai100@gmail.com>
2026-07-03 19:15:26 +0530

    Update cookie options for security enhancements

--- Backend/src/utils/cookieOptions.js
+++ Backend/src/utils/cookieOptions.js
@@ -1,12 +1,12 @@
 export const cookieOptions = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
-  maxAge: 7 * 24 * 60 * 60 * 1000
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 export const clearCookieOptions = {
   httpOnly: true,
-  secure: false,
+  secure: true,
   sameSite: "lax"
 };
